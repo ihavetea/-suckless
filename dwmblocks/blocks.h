b67243540465aa33fc085fd8569d40f7a43055c6
ihavetea <ihavetea@gmail.com>
2019-12-23 01:33:52 +0100

    autopush

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🔊 ", "amixer get Master | grep Left | grep -o \"\\(\\[\\of]\\|[0-9]*%\\)\" ", 60, 0},
+	{"🔊 ", "amixer get Master | grep Left | grep -o \"\\(\\[\\of]\\|[0-9]*%\\)\" ", 1, 0},
 	{"⧈ ", "sed \"s/$/%/\" /sys/class/power_supply/BAT0/capacity ", 5, 12},
 	{"⧉ ", "sed \"s/$/%/\" /sys/class/power_supply/BAT1/capacity ", 5, 12},
 	{"🕗 ", "date '+%b %d (%a) %I:%M%p'",	60,	0},
